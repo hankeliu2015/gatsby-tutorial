1a0dbfbf576cb5b38f62da140f58665dfc3c0bfc
Hank Liu <hanke.liu@gmail.com>
2020-10-02 12:14:42 -0400

    testing link components

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,17 @@
 
 import React, { Component } from 'react';
+import { Link } from 'gatsby';
 
 export default class blog extends Component {
 
   render() {
     return (
-      <div>this is our blog</div>
+      <div>
+        this is our blog
+        <div>
+          <Link to='/'>Home Page</Link>
+        </div>
+      </div>
     );
   }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
-import React from "react"
+import React from "react";
+import {Link} from 'gatsby';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      Hello world!
+      <div>
+        <Link to="/blog">Blog Page</Link>
+      </div>
+      <a href="https://www.gatsbyjs.org">Gatsby Docs</a>
+    </div>
+  )
 }
