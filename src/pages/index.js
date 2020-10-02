098d8f868625e3a95afcff88769c8d58dd945fe2
Hank Liu <hanke.liu@gmail.com>
2020-10-02 12:32:38 -0400

    add navbar component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import {Link} from 'gatsby';
+import Navbar from '../components/Navbar.js'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       Hello world!
       <div>
         <Link to="/blog">Blog Page</Link>
