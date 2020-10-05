c3fd7f6f3006679821b41ada4b160ac19a5be7d7
Hank Liu <hanke.liu@gmail.com>
2020-10-05 13:04:35 -0400

    set up styles for images component

--- src/examples/images.js
+++ src/examples/images.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import img from "../images/image1.jpg";
+import Image from "gatsby-image";
 
 const images = () => {
   return (
-    <div> hello from images</div>
+    <section className="images">
+      <article className="single-image">
+        <h3>basic image</h3>
+      </article>
+      <article className="single-image">
+        <h3>fixed image/blur</h3>
+      </article>
+      <article className="single-image">
+        <h3>fluid image/svg</h3>
+      </article>
+    </section>
   )
 };
 
